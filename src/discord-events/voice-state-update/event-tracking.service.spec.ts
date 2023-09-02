32ee90fe9cbd875808723c16edc96eef7c97c6e3
SlinkyPotato <patinobrian@gmail.com>
2023-09-02 10:28:05 -0400

    test: add first test for event-tracking.service.ts

--- src/discord-events/voice-state-update/event-tracking.service.spec.ts
+++ src/discord-events/voice-state-update/event-tracking.service.spec.ts
@@ -1,11 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventTrackingService } from './event-tracking.service';
-import {describe, expect, it, beforeEach, jest} from '@jest/globals';
-import {Logger} from "@nestjs/common";
-import {CACHE_MANAGER} from "@nestjs/cache-manager";
+import { describe, expect, it, beforeEach, jest } from '@jest/globals';
+import { Logger } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { VoiceState } from 'discord.js';
 
 describe('VoiceStateUpdateService', () => {
   let service: EventTrackingService;
+
+  const mockOldVoiceState = {
+    channelId: '123',
+    deaf: false,
+  };
+
+  const mockNewVoiceState = {
+    channelId: '123',
+    deaf: false,
+  };
 
   const mockCacheManager = {
     del: jest.fn().mockReturnThis(),
@@ -14,7 +25,11 @@ describe('VoiceStateUpdateService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventTrackingService, Logger, { provide: CACHE_MANAGER, useValue: mockCacheManager },],
+      providers: [
+        EventTrackingService,
+        Logger,
+        { provide: CACHE_MANAGER, useValue: mockCacheManager },
+      ],
     }).compile();
 
     service = module.get<EventTrackingService>(EventTrackingService);
@@ -23,4 +38,19 @@ describe('VoiceStateUpdateService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should ignore non-active event', async () => {
+    const spy = jest.spyOn(mockCacheManager, 'get');
+    spy.mockReturnValue(null);
+    try {
+      await service.handleParticipantTracking(
+        mockOldVoiceState as VoiceState,
+        mockNewVoiceState as VoiceState,
+      );
+    } catch (e) {}
+    expect(spy).toHaveBeenCalled();
+    expect(spy.mock.results[0].value).toBe(null);
+  });
+
+  // TODO: Add more tests
 });
