1ad67dfcf87fb5167b6a893cc128b03577b200e1
Brian Patino <Patinobrian@gmail.com>
2023-09-06 16:11:10 -0400

    test: add full test coverage for EventsProcessorService

--- src/processors/processors.module.ts
+++ src/processors/processors.module.ts
@@ -1,9 +1,8 @@
-import { ProcessorsService } from './processors.service';
 import { Module } from '@nestjs/common';
 import { EventsProcessorModule } from './events/events-processor.module';
 
 @Module({
   imports: [EventsProcessorModule],
-  providers: [ProcessorsService],
+  providers: [],
 })
 export class ProcessorsModule {}
