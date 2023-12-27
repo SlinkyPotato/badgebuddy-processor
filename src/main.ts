6507df4d14fd45ed45ec13962659b8496e4b7501
SlinkyPotato <patinobrian@gmail.com>
2023-12-27 12:25:12 -0500

    enhance sync script

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,7 @@ async function bootstrap() {
   context.enableShutdownHooks();
 }
 
-bootstrap()
-  .then(() => {
-    console.log('Processor started');
-  })
-  .catch((err) => {
-    console.error('Error starting processor', err);
-    process.exit(1);
-  });
+bootstrap().catch((err) => {
+  console.error('Error starting processor', err);
+  process.exit(1);
+});
